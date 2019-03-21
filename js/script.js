e011c68f663cc81528e5cafbf85a4d513447379e
George Pyott <george.pyott@gmail.com>
2019-03-21 14:17:28 +0000

    added more jquery to append remaining items

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-/*** requests xml feed from the .php file ***/
+    /*** requests xml feed from the .php file ***/
 
     var feed = "/bbsnewsfeed.php";
 
@@ -18,13 +18,27 @@ $(document).ready(function() {
 });
 
 
+function xmlLog(feed) {
+    console.log(feed);
+}
+
 /*** Appends content into divs ***/
 
 function xmlGet(feed) {
+    $("#load").fadeOut();
+    $("#load2").fadeOut();
+
     $(feed).find("item").each(function() {
-        $("#container").append('<div class="title" id="title">' + $(this).find("title").text() + '<div class="description" id="description">' + $(this).find("description").text() +  '<a href="#" class="link" id="link">' + $(this).find("link").text() + '</div>' + '</div>')
+        $("#container").append('<div class="title" id="title">' +
+            $(this).find("title").text() + '<div class="description" id="description">' + $(this).find("description").text() 
+            + '<br>' 
+            + '<img src="' + $(this).find("media:thumbnail").attr("url") + '" width="200" height="200"'+'/>'
+            + '<div class="date">' + $(this).find("pubDate").text() + '</div>'
+            + '<a  class="link" href="' + $(this).find("link").text() + '"' 
+            + 'target="_blank"' 
+            + '>' 
+            + '<span class="link-text">Read the full article</span>' + '</div>'
+            + '</div>');
     });
-    
-    
-    
+
 }
